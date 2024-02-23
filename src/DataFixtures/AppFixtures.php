dbe3834cc8953363620fc7cf91e31cdc4179e2d2
SachaBarbet <sacha.barbet@gmail.com>
2024-02-23 08:03:56 +0100

    add taste to fixtures, monolog package and .env credentials for public and admin users

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,11 +5,14 @@ namespace App\DataFixtures;
 use App\Entity\Bean;
 use App\Entity\Category;
 use App\Entity\Coffee;
+use App\Entity\Taste;
 use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
 use Faker\Factory;
 use Faker\Generator;
+use Psr\Log\LoggerInterface;
+use Symfony\Component\Dotenv\Dotenv;
 use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
@@ -25,42 +28,59 @@ class AppFixtures extends Fixture
      * @var UserPasswordHasherInterface
      */
     private UserPasswordHasherInterface $userPasswordHasher;
-    public function __construct(UserPasswordHasherInterface $userPasswordHasher) {
+
+    /**
+     * Dotenv
+     * @var Dotenv
+     */
+    private Dotenv $dotenv;
+
+    private LoggerInterface $logger;
+
+    public function __construct(UserPasswordHasherInterface $userPasswordHasher, LoggerInterface $loggerInterface) {
         $this->faker = Factory::create("fr_FR");
         $this->userPasswordHasher = $userPasswordHasher;
+        $this->dotenv = new Dotenv();
+        $this->logger = $loggerInterface;
     }
 
     public function load(ObjectManager $manager): void
     {
-        //Public
+        // Public
+        $publicUsername = $_ENV['PUBLIC_USER_ID'];
+        $publicPassword = $_ENV['PUBLIC_USER_PASSWORD'];
+
         $publicUser = new User();
-        $password = $this->faker->password(2,6);
         $publicUser
-            ->setUuid($this->faker->uuid() . "@" . $password)
-            ->setPassword($this->userPasswordHasher->hashPassword($publicUser, $password))
+            ->setUuid($publicUsername . "@" . $publicPassword)
+            ->setPassword($this->userPasswordHasher->hashPassword($publicUser, $publicPassword))
             ->setRoles(["ROLE_PUBLIC"]);
         $manager ->persist($publicUser);
             
-        for ($i=0;$i<10;$i++)
+        // Users
+        for ($i=0; $i<10; $i++)
         {
             $userUser = new User();
             $password = $this->faker->password(2,6);
             $userUser
-            ->setUuid($this->faker->uuid() . "@" . $password)
-            ->setPassword($this->userPasswordHasher->hashPassword($userUser,$password))
-            ->setRoles(["ROLE_PUBLIC"]);
-        $manager ->persist($userUser);
+                ->setUuid($this->faker->uuid() . "@" . $password)
+                ->setPassword($this->userPasswordHasher->hashPassword($userUser, $password))
+                ->setRoles(["ROLE_PUBLIC"]);
+            $manager ->persist($userUser);
         }
+
+        // Admin
+        $adminUsername = $_ENV['ADMIN_USER_ID'];
+        $adminPassword = $_ENV['ADMIN_USER_PASSWORD'];
 
         $adminUser = new User();
         $adminUser
-        ->setUuid("admin")
-        ->setPassword($this->userPasswordHasher->hashPassword($adminUser,"admin"))
-        ->setRoles(["ROLE_ADMIN"]);
+            ->setUuid($adminUsername)
+            ->setPassword($this->userPasswordHasher->hashPassword($adminUser, $adminPassword))
+            ->setRoles(["ROLE_ADMIN"]);
         $manager->persist($adminUser);
 
-        
-
+        // Category, Bean and Taste
         for ($j=0; $j < 20; $j++) {
             $category = new Category();
             $category->setName("category ". $j);
@@ -70,6 +90,7 @@ class AppFixtures extends Fixture
             $category->setUpdatedAt($updatedAt);
             $category->setStatus("active");
             $manager->persist($category);
+
             $bean = new Bean();
             $bean->setName("Bean ". $j);
             $bean->setOrigin("Pays ". $j);
@@ -79,10 +100,22 @@ class AppFixtures extends Fixture
             $bean->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in.". $j);
             $manager->persist($bean);
 
+            $taste = new Taste();
+            $taste->setName("Taste ". $j);
+            $taste->setDescription("desc ". $j);
+            $taste->setCaffeineRate($this->faker->randomFloat(2,0,1));
+            $taste->setIntensity($this->faker->randomInt(0,5));
+            $taste->setCreatedAt($createdAt);
+            $taste->setUpdatedAt($updatedAt);
+            $taste->setStatus("active");
+            $taste->setDescription("Lorem ipsum dolor sit amet, consectetur adipiscing elit. Maecenas in.". $j);
+            $manager->persist($taste);
+
+            // Coffee
             for ($i = 0; $i < 5; $i++) {
                 $ig = new Coffee();
-                $ig->setName("coffee ". $i+$j*5);
-                $ig->setDescription("desc ". $i+$j*5);
+                $ig->setName("coffee ". $i + $j * 5);
+                $ig->setDescription("desc ". $i + $j * 5);
                 $createdAt = $this->faker->dateTimeBetween("-1 week","now");
                 $updatedAt = $this->faker->dateTimeBetween($createdAt,"now");
                 $ig->setCreatedAt($createdAt);
